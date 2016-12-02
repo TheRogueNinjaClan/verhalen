195022e0aa9b7ede1fb81411d8777f4f9a0ba0b7
Oi Thuis <ocnninja@gmail.com>
2016-12-02 04:40:52 +0100

    animatie doet het

--- script/script.js
+++ script/script.js
@@ -1,11 +1,15 @@
 function showFilter(e){
   var targetClick = e.target.name;
-  var filterContainer = document.querySelector("#filter")
+  var filterContainer = document.querySelector(".filter")
 console.log(targetClick);
 
 if(targetClick == "filter"){
-  filterContainer.style.transform= "translate(-50vh, -50vw)";
+  filterContainer.style.top= "20vh";
   console.log('lol');
+}
+
+if(targetClick == "annuleren"){
+  filterContainer.style.top= "120vh";
 }
 }
 
